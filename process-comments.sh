94ecfaa1f4439b3bd99eef602c44076d08a1aca5
Michael Chary <mac389@gmail.com>
2016-05-21 14:09:12 -0400

    remove remote files not on local

--- process-comments.sh
+++
@@ -1,8 +0,0 @@
-## declare an array variable
-declare -a arr=("reporter" "interpreter" "manager" "superior")
-
-## now loop through the above array
-for condition in "${arr[@]}"
-do
-	tail -n +2 comments.csv | cut -d ',' -f 4,5 | grep -i $condition > comments-${condition}
-done
